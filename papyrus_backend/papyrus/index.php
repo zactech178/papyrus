e56682e157ebc01a52e7ece472380d1f6d301b64
zac <fullstacksoftwaredev@gmail.com>
2021-04-06 21:26:33 -0700

    minor fix

--- papyrus_backend/papyrus/index.php
+++ papyrus_backend/papyrus/index.php
@@ -4,5 +4,5 @@ header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
 header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 header ("Access-Control-Allow-Headers: *");
 
-//include_once('index.html');
+include_once('index.html');
 ?>
