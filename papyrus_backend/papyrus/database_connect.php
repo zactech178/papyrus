30027688abe062064d5fa190c33f0b5b2ff688db
zac <fullstacksoftwaredev@gmail.com>
2021-04-06 21:09:12 -0700

    minor change

--- papyrus_backend/papyrus/database_connect.php
+++ papyrus_backend/papyrus/database_connect.php
@@ -1,28 +1,12 @@
 <?php
 
-    $mysqlhost = "127.0.0.1";
-    function connect_to_db () {
-        global $mysqlhost;
-        $dbuser = "root";
-        $dbname = "papyrusbio";
-        $dbpass = "root";
-        $dbport = 8889;
-        $mysqli = new mysqli(
-            $mysqlhost,
-            $dbuser,
-            $dbpass,
-            $dbname,
-            $dbport
-        );
-        $GLOBALS['link'] = $mysqli;
-    }
-
+    // $mysqlhost = "127.0.0.1";
     // function connect_to_db () {
-    //     $mysqlhost = "mysql.papyrusbio.com:3306";
-    //     $dbuser = "dev_papyrusbio";
+    //     global $mysqlhost;
+    //     $dbuser = "root";
     //     $dbname = "papyrusbio";
-    //     $dbpass = "JswnV^919I%w";
-    //     $dbport = 3306;
+    //     $dbpass = "root";
+    //     $dbport = 8889;
     //     $mysqli = new mysqli(
     //         $mysqlhost,
     //         $dbuser,
@@ -32,5 +16,21 @@
     //     );
     //     $GLOBALS['link'] = $mysqli;
     // }
+
+    function connect_to_db () {
+        $mysqlhost = "mysql.papyrusbio.com:3306";
+        $dbuser = "dev_papyrusbio";
+        $dbname = "papyrusbio";
+        $dbpass = "JswnV^919I%w";
+        $dbport = 3306;
+        $mysqli = new mysqli(
+            $mysqlhost,
+            $dbuser,
+            $dbpass,
+            $dbname,
+            $dbport
+        );
+        $GLOBALS['link'] = $mysqli;
+    }
 
 ?>
